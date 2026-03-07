b43d58b730c63b51e129d7c933fc1f646cdbd2a9
AsgharAli1 <aasgharali1@gmail.com>
2026-03-07 21:43:38 +0000

    My Second Commit from singapore

--- mumbai.py
+++ mumbai.py
@@ -2,3 +2,5 @@ subtraction = num1 - num2
 print("Subtraction:", subtraction)
 multiplication = num1 * num2
 print("Multiplication:", multiplication)
+division = num1 / num2
+print("Division:", division)
